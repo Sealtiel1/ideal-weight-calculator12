8fc97e0595cffb57d68d8f8935d7986034c1e2b0
Sealtiel1 <sealtiel.dao191229@my.lsgh.edu.ph>
2021-04-30 22:17:08 +0800

    3 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -6,6 +6,7 @@ import { NavController, Select } from "ionic-angular";
 @Component({
   selector: "page-contact",
   templateUrl: "contact.html",
+   styleUrls: ['contact.css']
 })
 
 export class ContactPage {
